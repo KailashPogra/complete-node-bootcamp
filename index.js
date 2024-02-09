162ec61bfb70f602a50079dc3c826fcc8c53e3f1
Kailash <k11pogra@gmail.com>
2024-02-09 21:42:19 +0530

    11 lacture

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-const kailash = "jai hanuman ji , jai shree ram ji";
-console.log(kailash);
+
+const fs = require('fs');
+const http = require('http');
+//////////////////////////////
+////Files
+
+// blocking , synchronous technique
+// read file
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf8');
+// console.log(textIn);
+// write file
+// const textOut = `this is we know about avocado :${Date.now}:${textIn}`;
+// fs.writeFileSync('./txt/input.txt', textOut);
+// console.log('file written successfully');
+
+// non-blocking , Asynchronous technique
+  // in this example "file read successfully" print first then it return asynchronous function
+
+// fs.readFile('./txt/start.txt',"utf-8",(err, data1) => { 
+//     fs.readFile(`./txt/${data1}.txt`,"utf-8",(err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt',"utf-8",(err, data3) => {
+//             console.log(data3);
+//            fs.writeFile('./txt/final.text',`${data2}\n${data3}`,'utf-8',err=>{
+//             console.log(err);
+//             console.log('your file has been written');
+//            }); 
+//         }); 
+//     });
+// });
+// console.log('file read successfully');
+
+///////////////////////////
+/////////Sarver
+const Sarver = http.createServer((req, res)=>{
+  res.end('jai shree ram');
+});
+
+Sarver.listen(8000,'127.0.0.1',()=>{
+    console.log('Listning the request on 8000 port');
+})
